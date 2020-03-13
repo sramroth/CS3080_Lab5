afd36c7bb13c1a3cb83c7260462c9c9a16e2d61d
Scott <smramroth@gmail.com>
2020-03-13 12:06:25 -0600

    done

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def IsPrime(integer):
+	if integer > 1:
+		for number in range(2, integer):
+			if(integer % number) == 0:
+				return False
+			else:
+				continue
+		return True
+
+# Use nbrValues as the number of values to generate for Exercises 1-5
+nbrValues = int(input("What is the number of items you want to generate?\n"))
+
+###############################################################
+# Exercise 1 - Cubed Values from a Generator Object
+###############################################################
 class Iterator1():
 	def __init__(self, nbrValues):
 		self.i = 1
@@ -14,6 +29,16 @@ class Iterator1():
 		else:
 			raise StopIteration()
 
+print("%-16s" % "Cubes: ", end=' ')
+
+for i in Iterator1(nbrValues):
+	print(str(i), end=" ")
+
+print("\n")
+		
+###############################################################
+# Exercise 2 - Prime Values from a Generator Object
+###############################################################
 class Iterator2():
 	def __init__(self, nbrValues):
 		self.i = 0
@@ -36,56 +61,6 @@ class Iterator2():
 		else:
 			raise StopIteration()
 
-def IsPrime(integer):
-	if integer > 1:
-		for number in range(2, integer):
-			if(integer % number) == 0:
-				return False
-			else:
-				continue
-		return True
-
-def generator1(n):
-	i = 0
-	result = -1
-	while i < n:
-		yield result
-		i += 1
-		result -= 2
-
-def generator2(n):
-	i = 0
-	f1 = 1
-	f2 = 1
-	while i < n:
-		yield f1
-		temp = f1
-		f1 = f2
-		f2 = temp + f2
-		i += 1
-
-def generator3(n):
-	i = 1
-	while i <= n:
-		yield int((i * (i + 1)) / 2)
-		i += 1
-
-# Use nbrValues as the number of values to generate for Exercises 1-5
-nbrValues = int(input("What is the number of items you want to generate?\n"))
-
-###############################################################
-# Exercise 1 - Cubed Values from a Generator Object
-###############################################################
-print("%-16s" % "Cubes: ", end=' ')
-
-for i in Iterator1(nbrValues):
-	print(str(i), end=" ")
-
-print("\n")
-		
-###############################################################
-# Exercise 2 - Prime Values from a Generator Object
-###############################################################
 print("\n%-16s" % "Primes: ", end=' ')
 
 for i in Iterator2(nbrValues):
@@ -96,6 +71,14 @@ print("\n")
 ###############################################################
 # Exercise 3 - Odd Negative Values from a Generator Function
 ###############################################################
+def generator1(n):
+	i = 0
+	result = -1
+	while i < n:
+		yield result
+		i += 1
+		result -= 2
+
 print("\n%-16s" % "Odd Negatives: ", end=' ')
 
 for i in generator1(nbrValues):
@@ -107,6 +90,17 @@ print()
 ###############################################################
 # Exercise 4 - Fibonacci Values from a Generator Function
 ###############################################################
+def generator2(n):
+	i = 0
+	f1 = 1
+	f2 = 1
+	while i < n:
+		yield f1
+		temp = f1
+		f1 = f2
+		f2 = temp + f2
+		i += 1
+
 print("\n%-16s" % "Fibonacci: ", end=' ')
 
 for i in generator2(nbrValues):
@@ -118,6 +112,12 @@ print()
 ###############################################################
 # Exercise 5 - Triangular Values from a Generator Expression
 ###############################################################
+def generator3(n):
+	i = 1
+	while i <= n:
+		yield int((i * (i + 1)) / 2)
+		i += 1
+
 print("\n%-16s" % "Triangular: ", end=' ')
 
 for i in generator3(nbrValues):
@@ -131,48 +131,42 @@ print()
 ###############################################################
 print("\n%-16s" % "Ice Cream: ")
 
-# TODO: Make this function a Decorator
 def addBrownie(func):
 	def wrapper():
 		print("Adding Brownie")
 		func()
 	return wrapper
 
-# TODO: Make this function a Decorator
 def addBanana(func):
 	def wrapper():
 		print("Adding Banana")
 		func()
 	return wrapper
 
-# TODO: Make this function a Decorator
 def addSyrup(func):
 	def wrapper():
 		func()
 		print("Adding Chocolate Syrup")
 	return wrapper
 
-# TODO: Make this function a Decorator
 def addNuts(func):
 	def wrapper():
-		print("Adding Nuts")
 		func()
+		print("Adding Nuts")
 	return wrapper
 
-# TODO: Make this function a Decorator
 def addCherry(func):
 	def wrapper():
 		func()
 		print("Adding the Cherry on Top!")
 	return wrapper
 
-# TODO: Add Decorators to this function
-# DO NOT MODIFY ANYTHING BELOW THIS LINE
+@addCherry
+@addNuts
 @addBrownie
 @addBanana
 @addSyrup
-@addNuts
-@addCherry
+# DO NOT MODIFY ANYTHING BELOW THIS LINE
 def addIceCream():
 	print("Adding Ice Cream")
 
@@ -183,11 +177,30 @@ addIceCream()
 ###############################################################
 print("\n%-16s" % "Custom Toppings: ")
 
-# TODO: Implement a Decorator to add Base ingredients
+def addBase(ingredient):
+	def decoratorAddBase(func):
+		def wrapperAddBase(*args, **kwargs):
+			print(f"Adding {ingredient}")
+			func()
+		return wrapperAddBase
+	return decoratorAddBase
 
-# TODO: Implement a Decorator to add Topping ingredients
+def addTopping(ingredient):
+	def decoratorAddTopping(func):
+		def wrapperAddTopping(*args, **kwargs):
+			func()
+			if ingredient == "Cherry":
+				print(f"Adding the {ingredient} on Top!")
+			else:
+				print(f"Adding {ingredient}")
+		return wrapperAddTopping
+	return decoratorAddTopping
 
-# TODO: Add Decorators to this function with arguments
+@addBase(ingredient="Brownie")
+@addBase(ingredient="Banana")
+@addTopping(ingredient="Cherry")
+@addTopping(ingredient="Nuts")
+@addTopping(ingredient="Chocolate Syrup")
 # DO NOT MODIFY ANYTHING BELOW THIS LINE
 def addIceCream2():
 	print("Adding Ice Cream")
